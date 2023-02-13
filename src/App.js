d616eff3fc424f2ffff956b4fa1469f15999340d
dessimilate <kotpok03@gmail.com>
2023-02-13 13:47:20 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.scss'
+import ShoppingListElem from './components/ShoppingListElem.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
+  const [arrOfPlans, setArrOfPlans] = useState([''])
+
   return (
     <div className="App">
-
+      <h1>shopping list</h1>
+      <div className="shp-list">
+        {arrOfPlans.map((el, i) => 
+        <ShoppingListElem key={i} plan={el} setPlans={setArrOfPlans} index={i} />)}
+        <button onClick={() => setArrOfPlans(el => [...el, ''])}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/ShoppingListElem.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+const ShoppingListElem = ({ plan, setPlans, index }) => {
+  const [changeText, setChangeText] = useState(false)
+
+  return (
+    <>
+      <div className="shp-list-elem">
+        {changeText ?
+          <input
+            value={plan}
+            onChange={(e) => setPlans((el) => el.splice(index, 1, e.target.value))}
+            placeholder="enter text"
+            className="shp-list-elem-text"
+          /> : 
+          <p className="shp-list-elem-text">{plan === '' ? 'Planning...' : plan}</p>}
+        <div className="shp-list-elem-btns">
+          <button onClick={() => setChangeText((e) => !e)}>
+            <FontAwesomeIcon icon={changeText ? faCheck : faPenToSquare} />
+          </button>
+          <button onClick={() => setPlans(el => [...el.slice(0, index), ...el.slice(index+1)])}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ShoppingListElem
